aa6b17d6e11f1cb224ba94577fa3cff0e735bc2a
fatemehakbary82 <122644539+fatemehakbary82@users.noreply.github.com>
2023-11-12 17:00:18 +0330

    Update arrsaf.cpp

--- arrsaf.cpp
+++ arrsaf.cpp
@@ -1,96 +1,90 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
 using namespace std;
 
-const int Max_Size = 100;    //حداکثر تعداد عناصر در صف
-struct Queue
-{
-    int elements[Max_Size];  //آرایه اعداد صحیح برای ذخیره عناصر صف
-    int front;  //نشانگر ابتدای صف
-    int rear;  //نشانگر انتهای صف
+class Queue {
+private:
+    int* arr; // آرایه برای نگهداری اعضای صف
+    int capacity; // ظرفیت صف
+    int front; // اشاره‌گر به ابتدای صف
+    int rear; // اشاره‌گر به انتهای صف
+    int size; // تعداد اعضای صف
+
+public:
+    // سازنده
+    Queue(int capacity) {
+        this->capacity = capacity;
+        arr = new int[capacity];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+
+    // متد بررسی صف خالی بودن
+    bool isEmpty() {
+        return size == 0;
+    }
+
+    // متد بررسی صف پر بودن
+    bool isFull() {
+        return size == capacity;
+    }
+
+    // متد اضافه کردن به صف
+    void enqueue(int item) {
+        if (isFull()) {
+            std::cout << "صف پر است." <<endl;
+            return;
+        }
+
+        rear = (rear + 1) % capacity;
+        arr[rear] = item;
+        size++;
+        cout << "عنصر " << item << " به صف اضافه شد." << endl;
+    }
+
+    // متد حذف از ابتدای صف
+    void dequeue() {
+        if (isEmpty()) {
+            cout << "صف خالی است." << endl;
+            return;
+        }
+
+        int item = arr[front];
+        front = (front + 1) % capacity;
+        size--;
+        cout << "عنصر " << item << " از صف حذف شد." << endl;
+    }
+
+    // متد مشاهده عنصر ابتدای صف بدون حذف
+    int peek() {
+        if (isEmpty()) {
+            cout << "صف خالی است." << endl;
+            return -1; // یک مقدار دلخواه برای نشان‌دادن خطا
+        }
+
+        return arr[front];
+    }
+
+    // متد برعکس کردن صف
+    void reverse() {
+        if (isEmpty()) {
+            cout << "صف خالی است." <<endl;
+            return;
+        }
+
+        int* reversedArr = new int[capacity];
+        int index = 0;
+
+        for (int i = size - 1; i >= 0; i--) {
+            reversedArr[index++] = arr[i];
+        }
+
+        delete[] arr;
+        arr = reversedArr;
+
+        front = 0;
+        rear = size - 1;
+
+        cout << "صف برعکس شد." <<endl;
+    }
 };
-
-bool isEmpty(Queue& queue)   //تابع برسی صف خالی بودن
-{
-    return (queue.front == -1);
-}
-
-bool isFull(Queue& queue)   //تابع برسی صف پر بودن
-{
-    return(queue.rear == Max_Size - 1);
-}
-
-void Enqueue(Queue& queue , int element)  //تابع اضافه کردن به صف
-{
-    if(isFull(queue))
-    {
-        cout<< " صف پر است!"<<endl;
-    }
-    else
-    {
-        if(isEmpty(queue))
-        {
-            queue.front=0;
-        }
-        queue.rear++;
-        queue.elements[queue.rear]=element;
-        cout<< "عنصر"<<element<<"به صف اضافه شد."<<endl;
-    }
-}
-
-void Dequeue(Queue& queue)  //تابع حذف از ابتدای صف
-{
-    if(isEmpty(queue))
-    {
-        cout<< "صف خالی است!"<<endl;
-    }
-    else
-    {
-        int removedElement=queue.elements[queue.front];
-        if(queue.front==queue.rear)
-        {
-            queue.front=-1;
-            queue.rear=-1;
-        }
-        else
-        {
-            queue.front++;
-        }
-        cout<<"عنصر"<<removedElement<<"از ابتدای صف حذف شد."<<endl;
-    }
-}
-
-void reverseQueue(Queue& queue)  //تابع برعکس کردن صف
-{
-    if(isEmpty(queue))
-    {
-        cout<<"صف خالی است!"<<endl;
-    }
-    else
-    {
-        int i = queue.front;
-        int j = queue.rear;
-        while (i<j)
-        {
-           //جابه جایی عنصر
-           int temp = queue.elements[i];
-           queue.elements[i] = queue.elements[j];
-           queue.elements[j] = temp;
-           i++;
-           j--;
-        }
-        cout<<"صف برعکس شد."<<endl;
-    }
-}
-
-void peek(Queue& queue)   //تابع مشاهده گر از ابتدای صف بدون حذف
-{
-    if(isEmpty(queue))
-    {
-        cout<<"صف خالی است!"<<endl;
-    }
-    else
-    {
-        cout<<"عنصر ابتدای صف:"<<queue.elements[queue.front]<<endl;
-    }
-}
