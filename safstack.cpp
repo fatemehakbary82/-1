294430dae0f4c6ad1f8e5e51b4abf13b69e6c082
kianseretto2000 <150742154+kianseretto2000@users.noreply.github.com>
2023-11-17 14:21:25 +0330

    Update safstack.cpp

--- safstack.cpp
+++ safstack.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<stack>
+
 using namespace std;
 
 struct Queue {
@@ -10,6 +11,31 @@ struct Queue {
 	{
 		stack.push(x);
 	}
-    
 
+    // Dequeue an item from the queue
+	int dequeue()
+	{
+		if (stack.empty()) {
+			cout << "Queue is empty" << endl;;
+			exit(0);
+		}
+
+		// pop an item from the stack
+		int x = stack.top();
+		stack.pop();
+
+		// if stack becomes empty, return
+		// the popped item
+		if (stack.empty())
+			return x;
+
+		// recursive call
+		int ret = dequeue();
+
+		// push popped item back to the stack
+		stack.push(x);
+
+		// return the result of deQueue() call
+		return ret;
+	}
 };
