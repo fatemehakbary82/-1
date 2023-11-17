993058a8fb20f92d9d81cf3c11781ad3afd64764
fatemehakbary82 <122644539+fatemehakbary82@users.noreply.github.com>
2023-11-17 14:06:19 +0330

    Update stackqueue.cpp

--- stackqueue.cpp
+++ stackqueue.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
 #include<stack>
 using namespace std;
+ 
+struct Queue { 
+	stack<int> stack_; 
 
-struct Queue {
-	stack<int> stack1, stack2;
+	// Enqueue an item to the queue 
+	void enqueue(int x) 
+	{ 
+		stack_.push(x); 
+	} 
 
-	void enqueue(int x)
-	{
-		// Move all elements from s1 to s2
-		while (!stack1.empty()) {
-			stack2.push(stack1.top());
-			stack1.pop();
-		}
+	// Dequeue an item from the queue 
+	int dequeue() 
+	{ 
+		if (stack_.empty()) { 
+			cout << "Queue is empty" << endl;; 
+			exit(0); 
+		} 
 
-		// Push item into s1
-		stack1.push(x);
+		// pop an item from the stack 
+		int x = stack_.top(); 
+		stack_.pop(); 
 
-		// Push everything back to s1
-		while (!stack2.empty()) {
-			stack1.push(stack2.top());
-			stack2.pop();
-		}
-	}
+		// if stack becomes empty, return 
+		// the popped item 
+		if (stack_.empty()) 
+			return x; 
+
+		// recursive call 
+		int ret = dequeue(); 
+
+		// push popped item back to the stack 
+		stack_.push(x); 
+
+		// return the result of deQueue() call 
+		return ret; 
+	} 
+}; 
+
