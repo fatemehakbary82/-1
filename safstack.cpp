d7c38f8d9550041e4ed155e97c985d36b8c68fd7
fatemehakbary82 <122644539+fatemehakbary82@users.noreply.github.com>
2023-12-01 17:17:25 +0330

    اضافه شد

--- safstack.cpp
+++ safstack.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<stack>
+
 using namespace std;
 
 struct Queue {
@@ -8,8 +9,33 @@ struct Queue {
 	// Enqueue an item to the queue
     	void enqueue(int x)
 	{
-		stack.push(x);
+		stack.push(x);   // X به بشته اضافه شد
 	}
-    
 
+    // Dequeue an item from the queue
+	int dequeue()
+	{
+		if (stack.empty()) {
+			cout << "Queue is empty" << endl;  //پشته خالی است
+			exit(0);
+		}
+
+		// pop an item from the stack
+		int x = stack.top();
+		stack.pop();    //pop حذف کردن عنصر
+
+		// if stack becomes empty, return
+		// the popped item
+		if (stack.empty())
+			return x;
+
+		// recursive call
+		int ret = dequeue();     //ret رابطه بازگشتی
+
+		// push popped item back to the stack
+		stack.push(x);
+
+		// return the result of deQueue() call
+		return ret;
+	}
 };
